911e429309dc2bf6f71885f1d0704380c348f408
unknown <tothszilvia88@gmail.com>
2021-04-10 20:21:03 +0200

    first commit picibio project

---
+++ picibio/src/main/java/com/hobby/picibio/PicibioApplication.java
@@ -0,0 +1,13 @@
+package com.hobby.picibio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicibioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicibioApplication.class, args);
+	}
+
+}
